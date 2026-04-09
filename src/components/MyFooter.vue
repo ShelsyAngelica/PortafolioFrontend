4f2caf9ae7955538e8c9676814c2d5cf440297d1
Shelsy Garcia <shelsyangelik@gmail.com>
2026-04-09 12:00:10 -0500

    Modificacion de vista footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,20 +1,7 @@
 <template>
     <section class="section-footer" id="footer">
         <div class="info-footer">
-            <!-- <div class="card-info">
-                <p>© 2025 Shelsy Garcia. </p>
-                <p>Todos los derechos reservados.</p>
-            </div> -->
-
-            <div class="card-info-2">
-                <a class="icon-footer" href="https://www.linkedin.com/in/shelsy-angelica-garcia-laverde-275530232/" target="_blank" rel="noopener noreferrer">
-                    <i class="devicon-linkedin-plain i-footer"></i>
-                </a>
-
-                <a class="icon-footer" href="https://github.com/ShelsyAngelica" target="_blank" rel="noopener noreferrer">
-                    <i class="devicon-github-plain i-footer"></i>
-                </a>
-            </div>
+            <span>© 2024 Shelsy García. Todos los derechos reservados.</span>
         </div>
     </section>
 </template>
@@ -31,15 +18,9 @@
 
 }
 
-.i-footer{
-    font-size: 2rem;
+.info-footer{
     color: white;
+    font-size: 1rem;
 }
-
-.icon-footer{
-    text-decoration: none;
-    margin-right: 10px;
-}
-
 
 </style>
